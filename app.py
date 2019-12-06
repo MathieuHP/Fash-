167422e472defe13c50cb8dd36caa7b123c484fd
MathieuHP <mathieuhanquin@gmail.com>
2019-12-06 13:04:15 +0100

    route to load picture for rating

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify
-from keras import backend
+from flask import Flask, request, jsonify, send_file
+from tensorflow.keras import backend
 import os
 import cv2
+import pandas as pd
 
 #custom modules
 from ressources.user_class import User, new_user_id
@@ -36,65 +37,70 @@ FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 @app.route("/", methods= ["GET", "POST"])
 def index():
-	return'''-> /new_user :  create a new user <br /> 
-	-> /recommended_picture :  get recommended pictures list <br /> 
-	-> /ratings_update :  give a rating and push it to DB <br /> 
-	-> /user/<username> :  go to user page  <br /> 
-	'''
+    return'''-> /new_user :  create a new user <br /> 
+    -> /recommended_picture :  get recommended pictures list <br /> 
+    -> /ratings_update :  give a rating and push it to DB <br /> 
+    -> /user/<username> :  go to user page  <br /> 
+    '''
 
 
 @app.route("/new_user", methods=["POST"])
 def new_user():
-	user = User(new_user_id())
+    user = User(new_user_id())
 
-	#push data to DB
+    #push data to DB
 
-	return "user created"
+    return "user created"
 
 
 @app.route("/load_image_for_rating", methods=["POST"])
-@login_required # protect route => only logged users can access it
+# @login_required # protect route => only logged users can access it
 def load_image_for_rating():
 
-	# DATABASE_CONNECTION()
-	# ------------- get picture list for the user ------------- *
-	LIST_PATH = r"DB\reco_list.csv"
-	df = pd.read_csv(LIST_PATH)
-	user_id = 5
+    user_id = 5
+    # DATABASE_CONNECTION()
+    # ------------- get picture list for the user ------------- *
+    # LIST_PATH = r"DB\reco_list.csv"
+    # df = pd.read_csv(LIST_PATH)
 
-	if user_id in df.user_id:
-    	pictures_list = df.iloc[user_id,1]
-    else:
-		pictures_list = FIRST_PICTURES_LIST
-
-
-	backend.clear_session()
-	image = pictures_list.pop(0)
+    # if user_id in df.user_id:
+    #     pictures_list = df.iloc[user_id,1]
+    # else:
+    #     pictures_list = FIRST_PICTURES_LIST
+    pictures_list = create_recommended_pictures_list(user_id)
 
 
-	# ------------- send list back to DB and create new one if ------------- *
-	if len(picture_list) < 10:  # MULTITHREADING !!!!
+    backend.clear_session()
+    image = pictures_list.pop(0)
+    image = image[1]
 
-fswerfwsgrdx
-	image = cv2.imread( FILE_PATH + "/" + image)
 
-	return image #jsonify(image)
+    # ------------- send list back to DB and create new one if ------------- *
+    if len(pictures_list) < 10:  # MULTITHREADING !!!!
+        create_recommended_pictures_list(user_id)
+
+    img_folder_path = r"C:\Users\mathi\Desktop\Cronos\Fash!\Images"
+    image_path =  img_folder_path +r"\\" + image
+
+    return send_file(image_path, mimetype='image/gif') #jsonify(image)
 
 
 @app.route("/rate_image", methods=["POST"])
-@login_required 
+# @login_required 
 def rate_image():
-	json_data = request.get_json()
-	# DATABASE_CONNECTION()
-	# ------------- push to db : rating x image x user => to DB ------------- *
+    json_data = request.get_json()
+    print(json_data)
+    
+    # DATABASE_CONNECTION()
+    # ------------- push to db : rating x image x user => to DB ------------- *
 
-	return redirect(url_for('load_image_for_rating'))
+    return redirect(url_for('load_image_for_rating'))
 
 
 # @app.route("/train", methods = ["GET"])
 # def train():
-# 	model_cnn()
-# 	return("training completed")
+#     model_cnn()
+#     return("training completed")
 
 
 # @app.route('/user/<username>')
@@ -104,5 +110,5 @@ def rate_image():
 
 # run server
 if __name__ == "__main__":
-	app.run(host="127.0.0.1", port=5000, debug = True)
-#	app.run(host="0.0.0.0", port = os.environ["PORT"])
+    app.run(host="127.0.0.1", port=5000, debug = True)
+#    app.run(host="0.0.0.0", port = os.environ["PORT"])
